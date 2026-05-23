1ca17a0a29efe24ab2543601ee351291805edeb4
Prasanna Kumar <prasannaboga@gmail.com>
2026-05-23 12:40:47 +0530

    chore: update bundler version and expand test suite coverage

--- spec/project42_spec.rb
+++ spec/project42_spec.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 RSpec.describe Project42 do
-  it "has a version number" do
+  it "project has a version number 0.1.0" do
     expect(Project42::VERSION).not_to be nil
+    expect(Project42::VERSION).to be "0.1.0"
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(true).to eq(true)
+  end
+
+  it "ruby version 3.4.9" do
+    expect(RUBY_VERSION).to eq("3.4.9")
   end
 end
